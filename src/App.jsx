5bbb879580fe219b0d9d9aaeab8b32463b4fe46d
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-10-28 15:58:42 -0500

    feat: Add support type

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
+import { useRef, useState } from 'react'
+import styles from './App.module.css'
+
 export default function App () {
+  const inputRef = useRef(null)
+  const [currentText, setCurrentText] = useState('')
+
   return (
-    <div>
-      <h1>Alfred</h1>
-    </div>
+    <main onClick={() => inputRef.current.focus()}>
+      <form className={styles.inputWrapper}>
+        <strong className={styles.prompt}>Alfred&gt;</strong>
+        <span className={styles.input}>
+          {currentText}
+        </span>
+      </form>
+      <input
+        type="text"
+        ref={inputRef}
+        className={styles.inputElement}
+        autoComplete="off"
+        autoCorrect="off"
+        autoCapitalize="off"
+        spellCheck="false"
+        onChange={(event) => setCurrentText(event.target.value)}
+      />
+    </main>
   )
 }
