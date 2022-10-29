8dc24d53488f02a6876f3d99c4c9a32a5682b728
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-10-29 13:19:14 -0500

    Add command history and print for screen

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,48 @@ import styles from './App.module.css'
 export default function App () {
   const inputRef = useRef(null)
   const [currentText, setCurrentText] = useState('')
+  const [commandsHistory, setCommandHistory] = useState([])
+
+  function handleSubmit (event) {
+    event.preventDefault()
+
+    if (currentText === '') return
+    setCommandHistory(prev => prev.concat(currentText.trim()))
+    setCurrentText('')
+    console.log(`Input >> ${currentText}`)
+  }
 
   return (
     <main onClick={() => inputRef.current.focus()}>
-      <form className={styles.inputWrapper}>
+      <div>
+        {commandsHistory.map((command, index) => (
+          <div className={styles.inputWrapper} key={`command-${index}`}>
+            <strong className={styles.prompt}>&gt;</strong>
+            <span className={styles.input}>
+              {command}
+            </span>
+          </div>
+        ))}
+      </div>
+      <div className={styles.inputWrapper}>
         <strong className={styles.prompt}>Alfred&gt;</strong>
         <span className={styles.input}>
-          {currentText}
+          {currentText.replaceAll(' ', '\u00a0')}
         </span>
+        <span className={styles.caret}/>
+      </div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          ref={inputRef}
+          className={styles.inputElement}
+          autoComplete="off"
+          autoCorrect="off"
+          autoCapitalize="off"
+          spellCheck="false"
+          onChange={(event) => setCurrentText(event.target.value)}
+        />
       </form>
-      <input
-        type="text"
-        ref={inputRef}
-        className={styles.inputElement}
-        autoComplete="off"
-        autoCorrect="off"
-        autoCapitalize="off"
-        spellCheck="false"
-        onChange={(event) => setCurrentText(event.target.value)}
-      />
     </main>
   )
 }
