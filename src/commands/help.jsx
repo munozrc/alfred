8e133803e3db9cb5c092c3ce3e54844262040583
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-10-29 23:36:58 -0500

    Update help command

--- src/commands/help.jsx
+++ src/commands/help.jsx
@@ -3,6 +3,7 @@ export default function help () {
     <div>
       <strong>- Señor, estos son algunos de los comandos disponibles</strong>
       <p><strong>whoami</strong> - Obtendra informacion sobre su direccion IP</p>
+      <p><strong>clear</strong> - Limpiar su pantalla</p>
     </div>
   )
 }
