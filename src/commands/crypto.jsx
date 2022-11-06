097b6775bc10992f659de4f2f02f5cd843a12891
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-11-05 22:27:53 -0500

    Refactor and new dechiper to use playfair

--- src/commands/crypto.jsx
+++ src/commands/crypto.jsx
@@ -1,4 +1,4 @@
-import { getFixedPosOfLetter, getMatrizOfLettersWithKey, getPairOfLetters } from '../utils'
+import { getMatrizOfLettersWithKey, getPairOfLetters, getTextPlayFair } from '../utils'
 
 function simpleTranspositionCipher (letters = []) {
   let even = ''
@@ -40,107 +40,19 @@ function simpleTranspositionDecipher (letters = []) {
   )
 }
 
-function getRule (matrix = [], pairLetter = '') {
-  const m1 = pairLetter.charAt(0)
-  const m2 = pairLetter.charAt(1)
-
-  const posM1 = {}
-  const posM2 = {}
-
-  for (let row = 0; row < 5; row++) {
-    for (let column = 0; column < 5; column++) {
-      const letter = matrix[row * 5 + column]
-
-      if (letter === '(I/J)') {
-        if (m1 === 'I' || m1 === 'J') {
-          posM1.row = row
-          posM1.column = column
-        }
-
-        if (m2 === 'I' || m2 === 'J') {
-          posM2.row = row
-          posM2.column = column
-        }
-      } else {
-        if (letter === m1) {
-          posM1.row = row
-          posM1.column = column
-        }
-
-        if (letter === m2) {
-          posM2.row = row
-          posM2.column = column
-        }
-      }
-    }
-  }
-
-  const matrixWide = (value) => value + 1 > 4 ? 0 : value + 1
-
-  if (posM1.row === posM2.row) {
-    return matrix[posM1.row * 5 + matrixWide(posM1.column)] + matrix[posM2.row * 5 + matrixWide(posM2.column)]
-  } else if (posM1.column === posM2.column) {
-    return matrix[matrixWide(posM1.row) * 5 + posM1.column] + matrix[matrixWide(posM2.row) * 5 + posM2.column]
-  } else {
-    const dialogal = posM2.column - posM1.column
-    return matrix[posM1.row * 5 + (posM1.column + dialogal)] + matrix[posM2.row * 5 + (posM2.column - dialogal)]
-  }
-}
-
 function playFairCipher (letters = [], keyArg = '') {
   if (keyArg === '') return (<div><strong>La palabra clave</strong></div>)
 
   const matrizOfLetters = getMatrizOfLettersWithKey(keyArg)
   const pairOfLetters = getPairOfLetters(letters)
 
-  const result = []
-
-  for (let i = 0; i < pairOfLetters.length; i++) {
-    const firstLetter = pairOfLetters[i].charAt(0)
-    const secondLetter = pairOfLetters[i].charAt(1)
-
-    let posFirstLetter = {}
-    let posSecondLetter = {}
-    let posFirstEncryptedLetter = {}
-    let posSecondEncryptedLetter = {}
-
-    for (let row = 0; row < 5; row++) {
-      for (let column = 0; column < 5; column++) {
-        const letter = matrizOfLetters[row * 5 + column]
-        const isCompoundLetter = letter === '(I/J)'
-
-        if (isCompoundLetter && (firstLetter === 'I' || firstLetter === 'J')) {
-          posFirstLetter = { row, column }
-          continue
-        }
-
-        if (isCompoundLetter && (secondLetter === 'I' || secondLetter === 'J')) {
-          posSecondLetter = { row, column }
-          continue
-        }
-
-        if (letter === firstLetter) posFirstLetter = { row, column }
-        if (letter === secondLetter) posSecondLetter = { row, column }
-      }
-    }
-
-    if (posFirstLetter.row === posSecondLetter.row) {
-      posFirstEncryptedLetter = { row: posFirstLetter.row, column: getFixedPosOfLetter(posFirstLetter.column) }
-      posSecondEncryptedLetter = { row: posFirstLetter.row, column: getFixedPosOfLetter(posSecondLetter.column) }
-    } else if (posFirstLetter.column === posSecondLetter.column) {
-      posFirstEncryptedLetter = { row: getFixedPosOfLetter(posFirstLetter.row), column: posFirstLetter.column }
-      posSecondEncryptedLetter = { row: getFixedPosOfLetter(posSecondLetter.row), column: posSecondLetter.column }
-    } else {
-      const calcOppositePos = posSecondLetter.column - posFirstLetter.column
-      posFirstEncryptedLetter = { row: posFirstLetter.row, column: posFirstLetter.column + calcOppositePos }
-      posSecondEncryptedLetter = { row: posSecondLetter.row, column: posSecondLetter.column - calcOppositePos }
-    }
-
-    const firstEncryptedLetter = matrizOfLetters[posFirstEncryptedLetter.row * 5 + posFirstEncryptedLetter.column]
-    const secondEncryptedLetter = matrizOfLetters[posSecondEncryptedLetter.row * 5 + posSecondEncryptedLetter.column]
-
-    result.push(firstEncryptedLetter + secondEncryptedLetter)
+  const getNextPosLetter = (pos) => {
+    const maxPosOfMatrix = 4
+    const nextPos = pos + 1
+    return nextPos > maxPosOfMatrix ? 0 : nextPos
   }
+
+  const result = getTextPlayFair(pairOfLetters, matrizOfLetters, getNextPosLetter)
 
   return (
     <div>
@@ -159,7 +71,23 @@ function playFairDecipher (letters = [], keyArg = '') { // crypto -dpf HBTIDBHKM
   const matrizOfLetters = getMatrizOfLettersWithKey(keyArg)
   const pairOfLetters = getPairOfLetters(letters)
 
-  console.log({ pairOfLetters })
+  const getBeforePosLetter = (pos) => {
+    const minPosOfMatrix = 0
+    const beforePos = pos - 1
+    return beforePos < minPosOfMatrix ? 4 : beforePos
+  }
+
+  const result = getTextPlayFair(pairOfLetters, matrizOfLetters, getBeforePosLetter)
+
+  return (
+    <div>
+      <strong>Decifrado usando PlayFair</strong>
+      <p><strong>Clave: </strong>{keyArg}</p>
+      <p><strong>Matriz con clave: </strong>{matrizOfLetters.join(', ')}</p>
+      <p><strong>Letras: </strong>{pairOfLetters.join(', ')}</p>
+      <p><strong>Resultado: </strong>{result.join('')}</p>
+    </div>
+  )
 }
 
 export default function crypto (allArgs = []) {
--- src/utils.js
+++ src/utils.js
@@ -32,8 +32,58 @@ export function getPairOfLetters (letters = '') {
   return array
 }
 
-export function getFixedPosOfLetter (position) {
-  const maxPosOfMatrix = 4
-  const nextPos = position + 1
-  return nextPos > maxPosOfMatrix ? 0 : nextPos
+export function getTextPlayFair (pairOfLetters = [], matrizOfLetters = [], getPosOfLetter = () => {}) {
+  const result = []
+
+  for (let i = 0; i < pairOfLetters.length; i++) {
+    const firstLetter = pairOfLetters[i].charAt(0)
+    const secondLetter = pairOfLetters[i].charAt(1)
+
+    let posFirstLetter = {}
+    let posSecondLetter = {}
+    let posFirstEncryptedLetter = {}
+    let posSecondEncryptedLetter = {}
+
+    for (let row = 0; row < 5; row++) {
+      for (let column = 0; column < 5; column++) {
+        const letter = matrizOfLetters[row * 5 + column]
+        const isCompoundLetter = letter === '(I/J)'
+
+        if (isCompoundLetter && (firstLetter === 'I' || firstLetter === 'J')) {
+          posFirstLetter = { row, column }
+          continue
+        }
+
+        if (isCompoundLetter && (secondLetter === 'I' || secondLetter === 'J')) {
+          posSecondLetter = { row, column }
+          continue
+        }
+
+        if (letter === firstLetter) posFirstLetter = { row, column }
+        if (letter === secondLetter) posSecondLetter = { row, column }
+      }
+    }
+
+    posFirstEncryptedLetter = { row: posFirstLetter.row, column: posFirstLetter.column }
+    posSecondEncryptedLetter = { row: posSecondLetter.row, column: posSecondLetter.column }
+
+    if (posFirstLetter.row === posSecondLetter.row) {
+      posFirstEncryptedLetter.column = getPosOfLetter(posFirstLetter.column)
+      posSecondEncryptedLetter.column = getPosOfLetter(posSecondLetter.column)
+    } else if (posFirstLetter.column === posSecondLetter.column) {
+      posFirstEncryptedLetter.row = getPosOfLetter(posFirstLetter.row)
+      posSecondEncryptedLetter.row = getPosOfLetter(posSecondLetter.row)
+    } else {
+      const calcOppositePos = posSecondLetter.column - posFirstLetter.column
+      posFirstEncryptedLetter.column = posFirstLetter.column + calcOppositePos
+      posSecondEncryptedLetter.column = posSecondLetter.column - calcOppositePos
+    }
+
+    const firstEncryptedLetter = matrizOfLetters[posFirstEncryptedLetter.row * 5 + posFirstEncryptedLetter.column]
+    const secondEncryptedLetter = matrizOfLetters[posSecondEncryptedLetter.row * 5 + posSecondEncryptedLetter.column]
+
+    result.push(firstEncryptedLetter + secondEncryptedLetter)
+  }
+
+  return result
 }
